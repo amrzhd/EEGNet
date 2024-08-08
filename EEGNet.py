39e96b46042a293929c40a943070ee9df4b85149
Amir Zahedi <amirzahedi0@gmail.com>
2024-08-08 11:46:31 +0330

    Update EEGNet.py

--- EEGNet.py
+++ EEGNet.py
@@ -1,37 +1,36 @@
 import torch
 import torch.nn as nn
 
-class EEGNet(nn.Module): # EEGNET-8,2
-    def __init__(self,  chans=22, classes=4, time_points=257, f1=8, f2=16, d=2,
-                 dropoutRate=0.5, max_norm1=1, max_norm2=0.25):
-        super(EEGNet, self).__init__()
+class EEGNetModel(nn.Module): # EEGNET-8,2
+    def __init__(self, chans=22, classes=4, time_points=1001, temp_kernel=32,
+                 f1=16, f2=32, d=2, pk1=8, pk2=16, dropout_rate=0.5, max_norm1=1, max_norm2=0.25):
+        super(EEGNetModel, self).__init__()
         # Calculating FC input features
-        linear_input_size = (time_points//32)*f2
+        linear_size = (time_points//(pk1*pk2))*f2
 
         # Temporal Filters
         self.block1 = nn.Sequential(
-            nn.Conv2d(1, f1, (1, 32), padding='same', bias=False),
+            nn.Conv2d(1, f1, (1, temp_kernel), padding='same', bias=False),
             nn.BatchNorm2d(f1),
-            # nn.BatchNorm2d(f1, momentum=0.01, eps=1e-3),
         )
         # Spatial Filters
         self.block2 = nn.Sequential(
             nn.Conv2d(f1, d * f1, (chans, 1), groups=f1, bias=False), # Depthwise Conv
             nn.BatchNorm2d(d * f1),
             nn.ELU(),
-            nn.AvgPool2d((1, 4)),
-            nn.Dropout(dropoutRate)
+            nn.AvgPool2d((1, pk1)),
+            nn.Dropout(dropout_rate)
         )
         self.block3 = nn.Sequential(
             nn.Conv2d(d * f1, f2, (1, 16),  groups=f2, bias=False, padding='same'), # Separable Conv
             nn.Conv2d(f2, f2, kernel_size=1, bias=False), # Pointwise Conv
             nn.BatchNorm2d(f2),
             nn.ELU(),
-            nn.AvgPool2d((1, 8)),
-            nn.Dropout(dropoutRate)
+            nn.AvgPool2d((1, pk2)),
+            nn.Dropout(dropout_rate)
         )
         self.flatten = nn.Flatten()
-        self.fc = nn.Linear(linear_input_size, classes)
+        self.fc = nn.Linear(linear_size, classes)
 
         # Apply max_norm constraint to the depthwise layer in block2
         self._apply_max_norm(self.block2[0], max_norm1)
